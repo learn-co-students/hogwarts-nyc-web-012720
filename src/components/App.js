ac6824c35a7eac41f22192f2575e5949b77092fc
aprilfaithdays <aprilfaith.e@gmail.com>
2020-04-02 12:04:53 -0400

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -5,11 +5,52 @@ import hogs from "../porkers_data";
 import HelloWorld from "./HelloWorld";
 
 class App extends Component {
+
+  state = {
+    greased: false
+  }
+
+  renderHogs = () => {
+    return hogs.map( (hog, index) => 
+      <HelloWorld 
+        key={index}
+        name={hog.name}
+        greased={hog.greased}
+        weight={hog.weight}
+        highest_medal={hog['highest medal achieved']}
+      />
+    )
+  }
+
+  toggleGreased = () => {
+    this.setState ({
+      greased: !this.state.greased
+    })
+  }
+
+  renderGreased = () => {
+    const greasedHogs = hogs.filter( hog => hog.greased)
+    return greasedHogs.map( (hog, index) => {
+      // console.log(hog.greased)
+      <HelloWorld 
+        key={index}
+        name={hog.name}
+        greased={hog.greased}
+        weight={hog.weight}
+        highest_medal={hog['highest medal achieved']}
+      />
+    }
+    )
+  }
+
   render() {
+    console.log(this.renderGreased())
     return (
       <div className="App">
         <Nav />
-        <HelloWorld />
+        <button onClick={this.toggleGreased}>Greased</button>
+        {/* {this.renderGreased()} */}
+        {this.state.greased ? this.renderGreased() : this.renderHogs()}
       </div>
     );
   }
