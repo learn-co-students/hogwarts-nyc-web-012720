bd7c0e975015d6a2214639baf1b13236c741de68
mdiaz26 <mike.diaz006@gmail.com>
2020-04-02 11:22:38 -0400

    images working

--- src/components/HelloWorld.js
+++ src/components/HelloWorld.js
@@ -3,11 +3,18 @@ import Hog from "./hog.js"
 
 class HelloWorld extends React.Component {
 
+  // fetchImages = () => {
+  //   fetch(hogImg)
+  //   .then(response => response.json())
+  //   .then(images => {console.log(images)})
+  // }
+
   render() {
     return (
       <div>
         <h1>Hello World</h1>
         <div className="ui grid container">
+          {/* {this.fetchImages()} */}
           {this.props.hogs.map(hog => {return <Hog key={hog.name} data={hog}/>})}
         </div>
       </div>
